f578d37425698aa105b4f9e566ea4509a8aca5b5
Kyle Huskey <kylehuskey@gmail.com>
2022-05-04 17:37:55 -0400

    updated form reset

--- index.js
+++ index.js
@@ -1,13 +1,14 @@
 const enteredZip = document.querySelector('.city')
-const wxButton = document.querySelector('.get-wx')
+const wxButton = document.querySelector('.form-input')
 const testZ = document.querySelector('.zip')
 
 
-wxButton.addEventListener('click', function(e){
+wxButton.addEventListener('submit', function(e){
     e.preventDefault()
     const zipCode = testZ.value
     // console.log(zipCode)
-    wxButton.innerText = `Here's what the day looks like`
+    // wxButton.innerText = `Here's what the day looks like`
+    wxButton.reset()
 
 
 fetch(`https://api.openweathermap.org/data/2.5/weather?zip=${zipCode}&appid=2ccbaf615c50480240e4b467b4aa60f4`)
@@ -17,6 +18,7 @@ fetch(`https://api.openweathermap.org/data/2.5/weather?zip=${zipCode}&appid=2ccb
 .then(function(data){
     const cloudIcon = data.weather[0].icon
     const clouds = data.weather[0].description
+   
     const cloudInfo = `
     
     <blockquote><img src="http://openweathermap.org/img/wn/${cloudIcon}@2x.png">${clouds}</blockquote>
@@ -36,7 +38,7 @@ fetch(`https://api.openweathermap.org/data/2.5/weather?zip=${zipCode}&appid=2ccb
     const windInfo = `
     <blockquote>${wind} MPH</blockquote>
     `
-
+    // document.querySelector('.header').innerText = "Here's Your Requested Weather"
     document.querySelector('.card-text').innerHTML = cloudInfo
     document.querySelector('.info-2').innerHTML = tempInfo
     document.querySelector('.info-3').innerHTML = windInfo
